f3bffab188bdb3af7852b2701a16208419e33f36
nuranawsaf <67543345+nuranawsaf@users.noreply.github.com>
2020-12-07 21:50:28 +0600

    done

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Resume.css';
+import cv from '../../images/cv-01.png';
+import cv1 from '../../images/cv-02.png';
 
 const Pages = () => {
     return (
-        <div>
-            <h1>this is pages</h1>
-        </div>
+        <section>
+            <a class="logo navbar-brand text-danger desi" href="/home">Follio</a>
+            <div className="top">
+            <h1>My Resume</h1>
+            </div>
+            <div>
+            <a className="bg" target="_blank" href="https://drive.google.com/file/d/1Sj0aB2XebrfJ5GBxZOsrRuom-vaKR07U/view?usp=sharing">Download Resume</a>
+            </div>
+
+            <div className="cv-size">
+                <img src={cv} alt=""/>      
+            </div>
+                
+            <div className="cv-size">
+                <img src={cv1} alt=""/>
+            </div>
+        </section>
     );
 };
 
